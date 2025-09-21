a837d6887712da4b91b53b2f94b0dbe93ea98fc3
tieshagr <tieshagr@mac.com>
2025-09-21 21:53:11 +0300

    first Django project

---
+++ lab1/mygroup.py
@@ -0,0 +1,33 @@
+groupmates = [
+    {
+        "name": "Александр",
+        "surname": "Иванов",
+        "exams": ["Информатика", "ЭЭиС", "Web"],
+        "marks": [4, 3, 5]
+    },
+    {
+        "name": "Иван",
+        "surname": "Петров",
+        "exams": ["История", "АиГ", "КТП"],
+        "marks": [4, 4, 4]
+    },
+    {
+        "name": "Кирилл",
+        "surname": "Смирнов",
+        "exams": ["Философия", "ИС", "КТП"],
+        "marks": [5, 5, 5]
+    }
+]
+
+def print_students(students):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    for student in students:
+        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+
+def mean(marks: list) -> float:
+    return sum(marks) / len(marks) if marks else 0
+    
+def filter_students(students:list, average_mark:float) -> list:
+    return list(filter(lambda student: mean(student.get('marks', [])) > average_mark, students))
+
+print_students(filter_students(groupmates, 4.9))
